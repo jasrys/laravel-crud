4f4b4d1257a507c1bb913f9b5f1df1748d42a627
Jason Rys <jasonrys@gmail.com>
2018-08-19 17:17:08 -0400

    Reference the package provider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,18 @@
 namespace Jasrys\Crud\Test;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Jasrys\Crud\CrudServiceProvider;
 
 class TestCase extends OrchestraTestCase
 {
-    //
+    /**
+     * Load package service provider
+     * @param  \Illuminate\Foundation\Application $app
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            CrudServiceProvider::class,
+        ];
+    }
 }
