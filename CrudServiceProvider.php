851c1a50858de8e06ca198d540ddfa75b3366846
Jason Rys <jasonrys@gmail.com>
2018-08-19 17:15:40 -0400

    Add service provider to register command

---
+++ CrudServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Jasrys\Crud;
+
+use Illuminate\Support\ServiceProvider;
+use App\Console\Commands\MakeCrud;
+
+class MyPackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MakeCrud::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
